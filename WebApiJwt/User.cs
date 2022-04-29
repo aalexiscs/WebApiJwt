89dd41849292de117969a896aab497a48469e832
Alexis Contreras <aalexiscs@outlook.com>
2022-04-29 15:38:01 -0600

    JSON Web Token creation

---
+++ WebApiJwt/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebApiJwt
+{
+	public class User
+	{
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
